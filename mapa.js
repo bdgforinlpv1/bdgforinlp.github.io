f281d02fff482a494e276b11f57b123ccfc149d4
bdgforinlpv1 <148038137+bdgforinlpv1@users.noreply.github.com>
2023-10-20 11:34:24 -0400

    initial commit

--- mapa.js
+++ mapa.js
@@ -1,3 +1,5 @@
+//import { comafect } from "./capas/localidades_afec";
+
 let map=L.map('map').setView([-16.05, -68.68333],12)
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
@@ -11,4 +13,16 @@ var miniMap = new L.Control.MiniMap(osm2,
     minimized: false,
     position: "bottomleft"
     }).addTo(map);
+//configurar popup
+function popup(feature,layer){
+if(feature.properties && feature.properties.nom_comuni){
+layer.bindPopup("<strong>Comunidad: </strong"+ feature.properties.nom_comuni+"<br/>"+feature.properties.Tipo_de_Evento)}
+};
+//agregar capa en formato GJson
+L.geoJson(comafect).addTo(map);
+var comafectJS = L.geoJson(comafect,{
+onEachFeature: popup
+}).addTo(map);
 
+    // Navbar shrink function https://tile.openstreetmap.org/{z}/{x}/{y}.png
+//http://localhost:8080/geoserver/bdgforinlp/wms
